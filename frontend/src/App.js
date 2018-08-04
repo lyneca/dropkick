1a9c6e9a6ecfe8b9f1ba413b7dc15ffac3640ae6
Unknown <kurianskis@gmail.com>
2018-08-04 23:03:40 +1000

    Basic Search Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,11 +21,15 @@
 // export default App;
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
+import SearchBar from './components/SearchBar'
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
+        <NavBar 
+          // <SearchBar />
+        />
+        
       </div>
     )
   }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,15 +2,23 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
 const NavBar = () => {
+    const inputProps = {
+        step: 300,
+      };
     return(
         <div>
         <AppBar position="static">
-            <Toolbar>
-                <Typography variant="title" color="inherit">
-                React & Material-UI Sample Application
-                </Typography>
-            </Toolbar>
+            <Grid container justify = "center">
+                <Toolbar>
+                    <Typography variant="title" color="inherit" align = "center" >
+                    Enter a Course :
+                    </Typography>
+                    <TextField id="time" type="string" inputProps={inputProps} />
+                </Toolbar>
+            </Grid>
         </AppBar>
         </div>
     )
