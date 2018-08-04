c50473be8095a1c93c02179df5810544718bdc90
Lyneca <atraguliauniliantauthr@gmail.com>
2018-08-04 22:24:30 +1000

    Add index template

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    # The variable "key" is now availabe for use in the template.
+    return render(request, "viewer/index.html", context={"key": "value"})
+    #                              ^ template file
